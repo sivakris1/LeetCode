07b7617ee17f8ca3a4fcb77293e7947cdaf2eb3c
sivakrish1 <sivapalaparthi003@gmail.com>
2026-05-30 22:27:51 +0530

    solve(LeetCode-238): Product of Array Itself

---
+++ PrefixAndSuffix/238-product-of-array-itself.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+       
+       int[] left = new int[nums.length];
+       int[] right = new int[nums.length];
+       int[] ans = new int[nums.length];
+
+       left[0] = 1;
+       for(int i=1;i<left.length;i++){
+         left[i] = nums[i-1]*left[i-1] ;
+       }
+
+       right[right.length-1] = 1;
+       for(int i=right.length-2;i>=0;i--){
+         right[i] = nums[i+1] * right[i+1];
+       }
+
+       for(int i=0;i<nums.length;i++){
+        ans[i] = left[i] * right[i];
+       }
+
+
+
+       return ans;
+    }
+}
