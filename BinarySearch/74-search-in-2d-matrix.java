33b8fa79f661b17a37fa721c6039b4750533b256
sivakrish1 <sivapalaparthi003@gmail.com>
2026-06-14 21:36:46 +0530

    solve(LeetCode-74): Search in 2D Matrix

---
+++ BinarySearch/74-search-in-2d-matrix.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        
+        int left = 0;
+        //points to last element
+        int right = rows * cols - 1;
+
+        while(left <= right){
+            int mid = left + (right-left)/2;
+
+            //gives the present mid value element in the given 2D array
+            int row = mid / cols;
+            int col = mid % cols;
+
+            if(matrix[row][col] == target){
+                return true;
+            }
+
+            if(matrix[row][col] < target){
+                left = mid+1;
+            }
+            else{
+                right = mid-1;
+            }
+        }
+
+        return false;
+    }
+}
