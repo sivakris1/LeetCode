6dc5bea6e884f09621e64ba5874692d07dca20bd
sivakrish1 <sivapalaparthi003@gmail.com>
2026-07-07 21:44:24 +0530

    solve(LeetCode-226): Invert Binary Tree

--- Trees/226-invert-binary-tree.java
+++ Trees/226-invert-binary-tree.java
@@ -1,15 +1,21 @@
+
+//Trees solving using dfs
 class Solution {
     public TreeNode invertTree(TreeNode root) {
+        return swapNodes(root);
+    }
 
+    public TreeNode swapNodes(TreeNode root){
         if(root == null){
             return null;
         }
-        
-        TreeNode left = invertTree(root.left);
-        TreeNode right = invertTree(root.right);
 
-        root.right = left;
+        TreeNode left = swapNodes(root.left);
+        TreeNode right = swapNodes(root.right);
+
+        TreeNode temp = left;
         root.left = right;
+        root.right = temp;
 
         return root;
     }
