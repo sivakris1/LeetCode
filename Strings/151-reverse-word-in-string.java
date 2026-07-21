ac8f96b6bf4f58481d1e37b7646e72cd418ec761
sivakrish1 <sivapalaparthi003@gmail.com>
2026-07-21 23:06:24 +0530

    solve(LeetCode-151): Reverse word in String

---
+++ Strings/151-reverse-word-in-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String reverseWords(String s) {
+        String[] word = s.trim().split("\\s+");
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=word.length-1; i>=0; i--){
+            sb.append(word[i]);
+
+            if(i > 0){
+                sb.append(" ");
+            }
+        }
+
+        return sb.toString();
+    }
+}
