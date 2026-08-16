c9166d13266ebb1ad5d652bd9fe09838efff4288
sivakrish1 <sivapalaparthi003@gmail.com>
2026-08-16 23:28:01 +0530

    solve(LeetCode-2342): Max Sum of a Pair With Equal Sum of Digits

--- HashMap/2342-max-of-sum-with-equal-sum-of-digits.java
+++ HashMap/2342-max-of-sum-with-equal-sum-of-digits.java
@@ -30,3 +30,48 @@ class Solution {
         return sum;
     }
 }
+
+
+//approach 2: optimized one. where we use hashmap for saving the sum of individual numbers for finding pairs
+
+class Solution {
+    public int maximumSum(int[] nums) {
+        int max = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            int sum = sumDigit(nums[i]);
+
+            if(map.containsKey(sum)){
+                int idx = map.get(sum);
+
+                int totalSum = nums[idx] + nums[i];
+
+                max = Math.max(max, totalSum);
+
+                if(nums[i] > nums[idx]){
+                    map.put(sum, i);
+                }
+            }
+            else{
+                map.put(sum, i);
+            }
+        }
+
+        return max > 0 ? max : -1;
+        
+
+    }
+
+    public int sumDigit(int number){
+        int sum = 0;
+
+        while(number > 0){
+            int n = number % 10;
+            sum += n;
+            number = number/10;
+        }
+
+        return sum;
+    }
+}
