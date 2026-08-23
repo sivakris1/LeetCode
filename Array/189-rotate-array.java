aedd6b6ecb7684f0a2f86fb30b0c9b2e249d14bc
sivakrish1 <sivapalaparthi003@gmail.com>
2026-08-23 22:47:12 +0530

    solve(LeetCode-189): Rotate array

---
+++ Array/189-rotate-array.java
@@ -0,0 +1,23 @@
+
+//approach : new technique for rotation type questions ---> 
+// reverse total array and then reverse till k index and then reverse the remaining array. 
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+
+        reverse(nums, 0, nums.length-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, nums.length-1);
+    }
+
+    public void reverse(int[] nums, int left, int right){
+        while(left <= right){
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
