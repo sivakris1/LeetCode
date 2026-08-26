ed16b9cfd38e0039a38ffb02708e72d7237ecffc
sivakrish1 <sivapalaparthi003@gmail.com>
2026-08-26 23:16:11 +0530

    solve(LeetCode-3218): Smallest missing multiple of k

---
+++ Array/3218-smallest-missing-multiple-of-k.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            map.put(nums[i], 0);
+        }
+
+        for(int i=1; i<=100; i++){
+            if(!map.containsKey(k * i)){
+                return k * i;
+            }
+        }
+        return 101;
+    }
+}s
